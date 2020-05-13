6ba4152651e2ee67dcf842a479c05bae3882d0b3
elenh <yisiwings@163.com>
2020-05-13 14:18:38 +0800

    rebuild

--- src/SvgIcon/index.vue
+++ src/SvgIcon/index.vue
@@ -1,15 +1,20 @@
 <template>
-  <div v-if="isExternal" :style="styleExternalIcon" class="svg-external-icon svg-icon" v-on="$listeners"></div>
+  <div
+    v-if="isExternal"
+    :style="styleExternalIcon"
+    class="svg-external-icon svg-icon"
+    v-on="$listeners"
+  ></div>
   <svg v-else :class="svgClass" aria-hidden="true" v-on="$listeners">
     <use :xlink:href="iconName" />
   </svg>
 </template>
 
 <script>
-import { isExternal } from '../utils/validate'
+import { isExternal } from "../utils/validate";
 
 export default {
-  name: 'SvgIcon',
+  name: "AiSvgIcon",
   props: {
     iconClass: {
       type: String,
@@ -17,31 +22,31 @@ export default {
     },
     className: {
       type: String,
-      default: ''
+      default: ""
     }
   },
   computed: {
     isExternal() {
-      return isExternal(this.iconClass)
+      return isExternal(this.iconClass);
     },
     iconName() {
-      return `#icon-${this.iconClass}`
+      return `#icon-${this.iconClass}`;
     },
     svgClass() {
       if (this.className) {
-        return 'svg-icon ' + this.className
+        return "svg-icon " + this.className;
       } else {
-        return 'svg-icon'
+        return "svg-icon";
       }
     },
     styleExternalIcon() {
       return {
         mask: `url(${this.iconClass}) no-repeat 50% 50%`,
-        '-webkit-mask': `url(${this.iconClass}) no-repeat 50% 50%`
-      }
+        "-webkit-mask": `url(${this.iconClass}) no-repeat 50% 50%`
+      };
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -55,7 +60,7 @@ export default {
 
 .svg-external-icon {
   background-color: currentColor;
-  mask-size: cover!important;
+  mask-size: cover !important;
   display: inline-block;
 }
 </style>
